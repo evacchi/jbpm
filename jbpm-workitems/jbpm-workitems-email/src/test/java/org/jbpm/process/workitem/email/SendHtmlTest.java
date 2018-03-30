48e0bc379a712b2ef4dd939c4b345a85c7c49ed0
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-03-30 16:14:06 +0200

    add typed work item

--- jbpm-workitems/jbpm-workitems-email/src/test/java/org/jbpm/process/workitem/email/SendHtmlTest.java
+++ jbpm-workitems/jbpm-workitems-email/src/test/java/org/jbpm/process/workitem/email/SendHtmlTest.java
@@ -16,11 +16,8 @@
 
 package org.jbpm.process.workitem.email;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -33,11 +30,13 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.drools.core.process.instance.impl.DefaultWorkItemManager;
+import org.drools.core.process.instance.impl.TypedWorkItemImpl;
 import org.drools.core.process.instance.impl.WorkItemImpl;
 import org.jbpm.test.AbstractBaseTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.kie.api.runtime.process.TypedWorkItem;
 import org.kie.api.runtime.process.WorkItemManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,6 +49,10 @@ import org.subethamail.smtp.auth.PlainAuthenticationHandlerFactory;
 import org.subethamail.smtp.auth.UsernamePasswordValidator;
 import org.subethamail.wiser.Wiser;
 import org.subethamail.wiser.WiserMessage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class SendHtmlTest extends AbstractBaseTest {
 
@@ -64,7 +67,7 @@ public class SendHtmlTest extends AbstractBaseTest {
 
     private Random random = new Random();
     private int uniqueTestNum = -1;
-    
+
     private EmailWorkItemHandler emailWorkItemHandler = new EmailWorkItemHandler();
 
     @Before
@@ -74,8 +77,8 @@ public class SendHtmlTest extends AbstractBaseTest {
         emailHost = "localhost";
         int emailPortInt;
         do {
-            emailPortInt = random.nextInt((2*Short.MAX_VALUE-1));
-        } while( emailPortInt < 4096 );
+            emailPortInt = random.nextInt((2 * Short.MAX_VALUE - 1));
+        } while (emailPortInt < 4096);
 
         emailPort = Integer.toString(emailPortInt);
 
@@ -85,7 +88,7 @@ public class SendHtmlTest extends AbstractBaseTest {
 
     @After
     public void tearDown() throws Exception {
-        if( wiser != null ) {
+        if (wiser != null) {
             wiser.getMessages().clear();
             wiser.stop();
             wiser = null;
@@ -94,6 +97,7 @@ public class SendHtmlTest extends AbstractBaseTest {
 
     @SuppressWarnings("unused")
     private class ExtendedConnection extends Connection {
+
         private String extraField;
     }
 
@@ -103,52 +107,52 @@ public class SendHtmlTest extends AbstractBaseTest {
         Connection connB = new Connection();
 
         // null test
-        assertTrue( !connA.equals(null));
+        assertTrue(!connA.equals(null));
         // different class test
-        assertTrue( !connA.equals("og"));
+        assertTrue(!connA.equals("og"));
         // extended class test
         ExtendedConnection connExt = new ExtendedConnection();
-        assertTrue( !connA.equals(connExt) );
+        assertTrue(!connA.equals(connExt));
         // null fields test
-        assertTrue( connA.equals(connB));
+        assertTrue(connA.equals(connB));
 
         // all null vs filled field test
         connA.setHost("Human");
         connA.setPort("Skin");
         connA.setUserName("Viral");
         connA.setPassword("Protein Gate");
-        assertTrue( ! connA.equals(connB));
+        assertTrue(!connA.equals(connB));
 
         // filled field test
         connB.setHost(connA.getHost());
         connB.setPort(new String(connA.getPort()));
         connB.setUserName(connA.getUserName());
         connB.setPassword(connA.getPassword());
-        assertTrue( connA.equals(connB));
+        assertTrue(connA.equals(connB));
 
         // some null vs filled field test
         connA.setPassword(null);
         connB.setPassword(null);
-        assertTrue( connA.equals(connB));
+        assertTrue(connA.equals(connB));
 
         // boolean
         connA.setStartTls(true);
-        assertTrue( !connA.equals(connB));
+        assertTrue(!connA.equals(connB));
         connB.setStartTls(true);
-        assertTrue( connA.equals(connB));
+        assertTrue(connA.equals(connB));
         connB.setStartTls(false);
-        assertTrue( !connA.equals(connB));
+        assertTrue(!connA.equals(connB));
     }
 
     @Test
     public void verifyWiserServerWorks() throws Exception {
         // Input
         String testMethodName = Thread.currentThread().getStackTrace()[1].getMethodName();
-        String toAddress = "boyd@crowdergang.org";
+        Recipient toAddress = Recipient.to("boyd@crowdergang.org");
         String fromAddress = "rgivens@kty.us.gov";
 
         // Setup email
-        WorkItemImpl workItem = createEmailWorkItem(toAddress, fromAddress, testMethodName);
+        TypedWorkItem<Message> workItem = createEmailWorkItem(toAddress, fromAddress, testMethodName);
         Connection connection = new Connection(emailHost, emailPort);
 
         sendAndCheckThatMessagesAreSent(workItem, connection);
@@ -161,11 +165,11 @@ public class SendHtmlTest extends AbstractBaseTest {
 
         // Input
         String testMethodName = Thread.currentThread().getStackTrace()[1].getMethodName();
-        String toAddress = "rgivens@kty.us.gov";
+        Recipient toAddress = Recipient.to("rgivens@kty.us.gov");
         String fromAddress = "whawkins@kty.us.gov";
 
         // Setup email
-        WorkItemImpl workItem = createEmailWorkItem(toAddress, fromAddress, testMethodName);
+        TypedWorkItem<Message> workItem = createEmailWorkItem(toAddress, fromAddress, testMethodName);
         Connection connection = new Connection(emailHost, emailPort, authUsername, authPassword);
 
         sendAndCheckThatMessagesAreSent(workItem, connection);
@@ -178,11 +182,11 @@ public class SendHtmlTest extends AbstractBaseTest {
 
         // Input
         String testMethodName = Thread.currentThread().getStackTrace()[1].getMethodName();
-        String toAddress = "rgivens@kty.us.gov";
+        Recipient toAddress = Recipient.to("rgivens@kty.us.gov");
         String fromAddress = "whawkins@kty.us.gov";
 
         // Setup email
-        WorkItemImpl workItem = createEmailWorkItemWithAttachment(toAddress, fromAddress, testMethodName);
+        TypedWorkItem<Message> workItem = createEmailWorkItemWithAttachment(toAddress, fromAddress, testMethodName);
         Connection connection = new Connection(emailHost, emailPort, authUsername, authPassword);
 
         // send email
@@ -193,8 +197,8 @@ public class SendHtmlTest extends AbstractBaseTest {
         assertEquals(1, messages.size());
 
         MimeMessage message = messages.get(0).getMimeMessage();
-        assertEquals(workItem.getParameter("Subject"), message.getSubject());
-        assertTrue(Arrays.equals(InternetAddress.parse((String) workItem.getParameter("To")),
+        assertEquals(workItem.getParameters().getSubject(), message.getSubject());
+        assertTrue(Arrays.equals(InternetAddress.parse(workItem.getParameters().getRecipients().getRecipients().get(0).getEmail()),
                                  message.getRecipients(RecipientType.TO)));
 
         assertTrue(message.getContent() instanceof Multipart);
@@ -209,8 +213,6 @@ public class SendHtmlTest extends AbstractBaseTest {
 
             assertEquals("email.gif", bodyPart.getFileName());
         }
-
-
     }
 
     @Test
@@ -220,11 +222,11 @@ public class SendHtmlTest extends AbstractBaseTest {
 
         // Input
         String testMethodName = Thread.currentThread().getStackTrace()[1].getMethodName();
-        String toAddress = "mags@bennetstore.com";
+        Recipient recipient = Recipient.to("mags@bennetstore.com");
         String fromAddress = "rgivens@kty.us.gov";
 
-        checkBadAuthentication(toAddress, fromAddress, testMethodName, authUsername, "bad password");
-        checkBadAuthentication(toAddress, fromAddress, testMethodName, "badUserName", authPassword);
+        checkBadAuthentication(recipient, fromAddress, testMethodName, authUsername, "bad password");
+        checkBadAuthentication(recipient, fromAddress, testMethodName, "badUserName", authPassword);
     }
 
     @Test
@@ -234,37 +236,39 @@ public class SendHtmlTest extends AbstractBaseTest {
 
         // Input
         String testMethodName = Thread.currentThread().getStackTrace()[1].getMethodName();
-        String toAddress = "rgivens@yahoo.com";
+        Recipient toAddress = Recipient.to("rgivens@yahoo.com");
         String fromAddress = "rgivens@kty.us.gov";
 
         EmailWorkItemHandler handler = new EmailWorkItemHandler();
-        handler.setConnection( emailHost, emailPort, authUsername, authPassword );
+        handler.setConnection(emailHost, emailPort, authUsername, authPassword);
 
-        WorkItemImpl workItem = new WorkItemImpl();
-        workItem.setParameter( "To", toAddress );
-        workItem.setParameter( "From", fromAddress );
-        workItem.setParameter( "Reply-To", fromAddress );
-        workItem.setParameter( "Subject", "Test mail for " + testMethodName );
-        workItem.setParameter( "Body", "Don't forget to check on Boyd later today." );
+        Message message = new Message();
+        TypedWorkItem<Message> workItem = new TypedWorkItemImpl<>(message);
+
+        message.getRecipients().addRecipient(toAddress);
+        message.setFrom(fromAddress);
+        message.setReplyTo(fromAddress);
+        message.setSubject("Test mail for " + testMethodName);
+        message.setBody("Don't forget to check on Boyd later today.");
 
         WorkItemManager manager = new DefaultWorkItemManager(null);
-        handler.executeWorkItem( workItem, manager );
+        handler.executeWorkItem(workItem, manager);
 
         List<WiserMessage> messages = wiser.getMessages();
         assertEquals(1, messages.size());
 
-        for (WiserMessage wiserMessage : messages ) {
-            MimeMessage message = wiserMessage.getMimeMessage();
-            assertEquals(workItem.getParameter("Subject"), message.getSubject());
-            assertTrue(Arrays.equals(InternetAddress.parse(toAddress),
-                                     message.getRecipients(RecipientType.TO)));
+        for (WiserMessage wiserMessage : messages) {
+            MimeMessage mimeMessage = wiserMessage.getMimeMessage();
+            assertEquals(workItem.getParameters().getSubject(), mimeMessage.getSubject());
+            assertTrue(Arrays.equals(InternetAddress.parse(toAddress.getEmail()),
+                                     mimeMessage.getRecipients(RecipientType.TO)));
         }
     }
 
     /**
      * Helper methods
      */
-    private void sendAndCheckThatMessagesAreSent(WorkItemImpl workItem, Connection connection) throws Exception {
+    private void sendAndCheckThatMessagesAreSent(TypedWorkItem<Message> workItem, Connection connection) throws Exception {
         // send email
         Email email = emailWorkItemHandler.createEmail(workItem, connection);
         SendHtml.sendHtml(email, connection);
@@ -272,19 +276,18 @@ public class SendHtmlTest extends AbstractBaseTest {
         List<WiserMessage> messages = wiser.getMessages();
         assertEquals(1, messages.size());
 
-        for (WiserMessage wiserMessage : messages ) {
+        for (WiserMessage wiserMessage : messages) {
             MimeMessage message = wiserMessage.getMimeMessage();
-            assertEquals(workItem.getParameter("Subject"), message.getSubject());
-            assertTrue(Arrays.equals(InternetAddress.parse((String) workItem.getParameter("To")),
+            assertEquals(workItem.getParameters().getSubject(), message.getSubject());
+            assertTrue(Arrays.equals(InternetAddress.parse(workItem.getParameters().getRecipients().getRecipients().get(0).getEmail()),
                                      message.getRecipients(RecipientType.TO)));
         }
-
     }
 
-    private void checkBadAuthentication(String toAddress, String fromAddress, String testMethodName,
+    private void checkBadAuthentication(Recipient toAddress, String fromAddress, String testMethodName,
                                         String username, String password) {
         // Setup email
-        WorkItemImpl workItem = createEmailWorkItem(toAddress, fromAddress, testMethodName);
+        TypedWorkItem<Message> workItem = createEmailWorkItem(toAddress, fromAddress, testMethodName);
         Connection connection = new Connection(emailHost, emailPort, username, password);
 
         // send email
@@ -292,48 +295,55 @@ public class SendHtmlTest extends AbstractBaseTest {
         try {
             SendHtml.sendHtml(email, connection);
         } catch (Throwable t) {
-            assertTrue( "Unexpected exception of type " + t.getClass().getSimpleName()
-                                + ", not " + t.getClass().getSimpleName(), (t instanceof RuntimeException));
+            assertTrue("Unexpected exception of type " + t.getClass().getSimpleName()
+                               + ", not " + t.getClass().getSimpleName(), (t instanceof RuntimeException));
             assertNotNull("Expected RuntimeException to have a cause.", t.getCause());
             Throwable cause = t.getCause();
             assertNotNull("Expected cause to have a cause.", cause.getCause());
             cause = cause.getCause();
-            assertTrue( "Unexpected exception of type " + cause.getClass().getSimpleName()
-                                + ", not " + cause.getClass().getSimpleName(), (cause instanceof AuthenticationFailedException));
+            assertTrue("Unexpected exception of type " + cause.getClass().getSimpleName()
+                               + ", not " + cause.getClass().getSimpleName(), (cause instanceof AuthenticationFailedException));
         }
     }
 
-    private WorkItemImpl createEmailWorkItem(String toAddress, String fromAddress, String testMethodName) {
-        WorkItemImpl workItem = new WorkItemImpl();
-        workItem.setParameter( "To", toAddress );
-        workItem.setParameter( "From", fromAddress );
-        workItem.setParameter( "Reply-To", fromAddress );
+    private TypedWorkItem<Message> createEmailWorkItem(Recipient toAddress, String fromAddress, String testMethodName) {
+        Message message = new Message();
+        TypedWorkItemImpl<Message> workItem = new TypedWorkItemImpl<>(message);
+        message.getRecipients().addRecipient(toAddress);
+        message.setFrom(fromAddress);
+        message.setReplyTo(fromAddress);
 
         String subject = this.getClass().getSimpleName() + " test message [" + uniqueTestNum + "]";
         String body = "\nThis is the test message generated by the " + testMethodName + " test (" + uniqueTestNum + ").\n";
-        workItem.setParameter( "Subject", subject );
-        workItem.setParameter( "Body", body );
+        message.setSubject(subject);
+        message.setBody(body);
 
         return workItem;
     }
 
-    private WorkItemImpl createEmailWorkItemWithAttachment(String toAddress, String fromAddress, String testMethodName) {
-        WorkItemImpl workItem = new WorkItemImpl();
-        workItem.setParameter( "To", toAddress );
-        workItem.setParameter( "From", fromAddress );
-        workItem.setParameter( "Reply-To", fromAddress );
+    private TypedWorkItem<Message> createEmailWorkItemWithAttachment(Recipient toAddress, String fromAddress, String testMethodName) {
+        TypedWorkItemImpl<Message> workItem = new TypedWorkItemImpl<>();
+        Message message = new Message();
+        workItem.setParameters(message);
+
+        message.getRecipients().addRecipient(toAddress);
+
+        message.setFrom(fromAddress);
+        message.setReplyTo(fromAddress);
 
         String subject = this.getClass().getSimpleName() + " test message [" + uniqueTestNum + "]";
         String body = "\nThis is the test message generated by the " + testMethodName + " test (" + uniqueTestNum + ").\n";
-        workItem.setParameter( "Subject", subject );
-        workItem.setParameter( "Body", body );
-        workItem.setParameter("Attachments", "classpath:/icons/email.gif");
+
+        message.setSubject(subject);
+        message.setBody(body);
+
+        message.setAttachments(Collections.singletonList("classpath:/icons/email.gif"));
 
         return workItem;
     }
-
 
     private static class TestAuthHandlerFactory implements AuthenticationHandlerFactory {
+
         MultipleAuthenticationHandlerFactory authHandleFactory = new MultipleAuthenticationHandlerFactory();
 
         public TestAuthHandlerFactory() {
@@ -357,6 +367,5 @@ public class SendHtmlTest extends AbstractBaseTest {
             return authHandleFactory.getAuthenticationMechanisms();
         }
     }
-
 }
 
