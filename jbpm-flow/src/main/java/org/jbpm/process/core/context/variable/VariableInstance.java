be284791d3decb944ee580186f68a2d7bdb74980
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-07-07 12:18:36 +0200

    better variable instance api

--- jbpm-flow/src/main/java/org/jbpm/process/core/context/variable/VariableInstance.java
+++ jbpm-flow/src/main/java/org/jbpm/process/core/context/variable/VariableInstance.java
@@ -1,129 +1,191 @@
 package org.jbpm.process.core.context.variable;
 
-import java.util.Collection;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import org.drools.core.ClassObjectFilter;
-import org.drools.core.event.ProcessEventSupport;
-import org.jbpm.process.core.Process;
-import org.jbpm.process.core.impl.ProcessImpl;
-import org.jbpm.process.instance.InternalProcessRuntime;
-import org.jbpm.process.instance.ProcessInstance;
-import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.process.CaseData;
-import org.kie.api.runtime.rule.FactHandle;
+import org.jbpm.process.instance.context.variable.VariableScopeInstance;
 
 public interface VariableInstance<T> {
-
-    String name();
 
     T get();
 
     void set(T value);
 
-    static <T> VariableInstance<T> of(ProcessInstance processInstance,
-                                      VariableScope variableScope,
-                                      String name,
-                                      Supplier<T> getter,
-                                      Consumer<T> setter) {
-        return new VariableInstance<T>() {
-            private transient String variableIdPrefix = null;
-            private transient String variableInstanceIdPrefix = null;
+    static <T> VariableInstance<T> of(
+            Supplier<T> getter,
+            Consumer<T> setter) {
+        return new LambdaVariableInstance<>(getter, setter);
+    }
 
-            ProcessEventSupport processEventSupport =
-                    ((InternalProcessRuntime) processInstance
-                            .getKnowledgeRuntime()
-                            .getProcessRuntime())
-                            .getProcessEventSupport();
+    static <T> VariableInstance<T> of(
+            VariableScopeInstance parentScope,
+            Variable variable) {
+        return new RootVariableInstance<>(parentScope, variable);
+    }
 
+    class RootVariableInstance<T> implements VariableInstance<T> {
 
+        private final Variable variableDescriptor;
+        private final VariableScopeInstance parentScopeInstance;
+        private VariableInstance<T> delegate;
 
-            @Override
-            public String name() {
-                return name;
-            }
+        public RootVariableInstance(VariableScopeInstance parentScopeInstance, Variable variableDescriptor) {
+            this.parentScopeInstance = parentScopeInstance;
+            this.variableDescriptor = variableDescriptor;
+            this.delegate = new SimpleVariableInstance<T>((T) variableDescriptor.getValue());
+        }
 
-            @Override
-            public T get() {
-                return getter.get();
-            }
+        public String name() {
+            return variableDescriptor.getName();
+        }
 
-            @Override
-            public void set(T value) {
-                beforeSetter(value);
-                T oldValue = get();
-                setter.accept(value);
-                afterSetter(oldValue, value);
-            }
+        public VariableScopeInstance getParentScopeInstance() {
+            return parentScopeInstance;
+        }
 
-            private void beforeSetter(T value) {
-                variableScope
-                        .validateVariable(
-                                ((Process) variableScope.getContextContainer()).getName(),
-                                name,
-                                value);
+        @Override
+        public T get() {
+            return delegate.get();
+        }
 
-                if (name == null) {
-                    throw new IllegalArgumentException(
-                            "The name of a variable may not be null!");
-                }
-                Object oldValue = get();
-                if (oldValue == null) {
-                    if (value == null) {
-                        return;
-                    }
-                }
-                processEventSupport.fireBeforeVariableChanged(
-                        prefixed(name),
-                        instancePrefixed(name),
-                        oldValue,
-                        value,
-                        processInstance,
-                        processInstance.getKnowledgeRuntime());
+        @Override
+        public void set(T value) {
+            delegate.set(value);
+        }
 
-                if (name.startsWith(VariableScope.CASE_FILE_PREFIX)) {
-                    String nameInCaseFile = name.replaceFirst(VariableScope.CASE_FILE_PREFIX, "");
-                    // store it under case file rather regular variables
-                    @SuppressWarnings("unchecked")
-                    Collection<CaseData> caseFiles = (Collection<CaseData>) processInstance.getKnowledgeRuntime().getObjects(new ClassObjectFilter(CaseData.class));
-                    if (caseFiles.size() == 1) {
-                        CaseData caseFile = (CaseData) caseFiles.iterator().next();
-                        FactHandle factHandle = processInstance.getKnowledgeRuntime().getFactHandle(caseFile);
-
-                        caseFile.add(nameInCaseFile, value);
-                        processInstance.getKnowledgeRuntime().update(factHandle, caseFile);
-                        ((KieSession)processInstance.getKnowledgeRuntime()).fireAllRules();
-                        return;
-                    }
-
-                }
-            }
-
-            public void afterSetter(T oldValue, T value) {
-                processEventSupport.fireAfterVariableChanged(
-                        prefixed(name),
-                        instancePrefixed(name),
-                        oldValue,
-                        value,
-                        processInstance,
-                        processInstance.getKnowledgeRuntime());
-
-            }
-
-
-            private String instancePrefixed(String name) {
-                return (variableInstanceIdPrefix == null? "" : variableInstanceIdPrefix + ":") + name;
-            }
-
-            private String prefixed(String name) {
-                return (variableIdPrefix == null ? "" : variableIdPrefix + ":") + name;
-            }
-
-
-
-        };
+        public void setDelegate(VariableInstance<T> delegate) {
+            this.delegate = delegate;
+        }
     }
 }
 
+class LambdaVariableInstance<T> implements VariableInstance<T> {
+
+    private final Supplier<T> getter;
+    private final Consumer<T> setter;
+
+    public LambdaVariableInstance(Supplier<T> getter, Consumer<T> setter) {
+        this.getter = getter;
+        this.setter = setter;
+    }
+
+    @Override
+    public T get() {
+        return getter.get();
+    }
+
+    @Override
+    public void set(T value) {
+        setter.accept(value);
+    }
+}
+
+
+
+class SimpleVariableInstance<T> implements VariableInstance<T> {
+
+    private T value;
+
+    public SimpleVariableInstance(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public void set(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public T get() {
+        return value;
+    }
+}
+
+//class ValidatedVariableInstanceDecorator<T> implements VariableInstance<T> {
+//
+//    ProcessInstance processInstance;
+//    VariableScope variableScope;
+//    VariableInstance<T> delegate;
+//    private transient String variableIdPrefix = null;
+//    private transient String variableInstanceIdPrefix = null;
+//
+//    ProcessEventSupport processEventSupport =
+//            ((InternalProcessRuntime) processInstance
+//                    .getKnowledgeRuntime()
+//                    .getProcessRuntime())
+//                    .getProcessEventSupport();
+//
+//    @Override
+//    public T get() {
+//        return delegate.get();
+//    }
+//
+//    @Override
+//    public void set(T value) {
+//        beforeSetter(value);
+//        T oldValue = get();
+//        delegate.set(value);
+//        afterSetter(oldValue, value);
+//    }
+//
+//    private void beforeSetter(T value) {
+//        variableScope
+//                .validateVariable(
+//                        ((Process) variableScope.getContextContainer()).getName(),
+//                        name(),
+//                        value);
+//
+//        if (name() == null) {
+//            throw new IllegalArgumentException(
+//                    "The name of a variable may not be null!");
+//        }
+//        Object oldValue = get();
+//        if (oldValue == null) {
+//            if (value == null) {
+//                return;
+//            }
+//        }
+//        processEventSupport.fireBeforeVariableChanged(
+//                prefixed(name()),
+//                instancePrefixed(name()),
+//                oldValue,
+//                value,
+//                processInstance,
+//                processInstance.getKnowledgeRuntime());
+//
+//        if (name().startsWith(VariableScope.CASE_FILE_PREFIX)) {
+//            String nameInCaseFile = name().replaceFirst(VariableScope.CASE_FILE_PREFIX, "");
+//            // store it under case file rather regular variables
+//            @SuppressWarnings("unchecked")
+//            Collection<CaseData> caseFiles = (Collection<CaseData>) processInstance.getKnowledgeRuntime().getObjects(new ClassObjectFilter(CaseData.class));
+//            if (caseFiles.size() == 1) {
+//                CaseData caseFile = (CaseData) caseFiles.iterator().next();
+//                FactHandle factHandle = processInstance.getKnowledgeRuntime().getFactHandle(caseFile);
+//
+//                caseFile.add(nameInCaseFile, value);
+//                processInstance.getKnowledgeRuntime().update(factHandle, caseFile);
+//                ((KieSession) processInstance.getKnowledgeRuntime()).fireAllRules();
+//                return;
+//            }
+//        }
+//    }
+//
+//    public void afterSetter(T oldValue, T value) {
+//        processEventSupport.fireAfterVariableChanged(
+//                prefixed(name()),
+//                instancePrefixed(name()),
+//                oldValue,
+//                value,
+//                processInstance,
+//                processInstance.getKnowledgeRuntime());
+//    }
+//
+//    private String instancePrefixed(String name) {
+//        return (variableInstanceIdPrefix == null ? "" : variableInstanceIdPrefix + ":") + name;
+//    }
+//
+//    private String prefixed(String name) {
+//        return (variableIdPrefix == null ? "" : variableIdPrefix + ":") + name;
+//    }
+//}
 
